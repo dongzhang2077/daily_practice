314dcec671a84270475b3c1a7f4c32973be8dcea
dong zhang <windyheratz5201314@gmail.com>
2026-04-14 23:01:31 -0700

    Add solution: Disemvowel_Trolls (7kyu) - 2026-04-14

---
+++ solutions/2026-04-14/Disemvowel_Trolls.js
@@ -0,0 +1,39 @@
+/*
+Problem: Disemvowel_Trolls
+Difficulty: 7kyu
+Date: 2026-04-14
+*/
+
+/*Disemvowel Trolls
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.*/
+
+
+
+//my solution:
+function disemvowel(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  
+  return str.split("")
+  .filter( c => !vowels.includes(c))
+  .join("");
+}
+//Sample Tests
+
+const chai = require('chai'), { assert } = chai;
+chai.config.truncateThreshold;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(disemvowel("This website is for losers LOL!"), "Ths wbst s fr lsrs LL!")
+    assert.strictEqual(disemvowel("No offense but,\nYour writing is among the worst I've ever read"), "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd")
+    assert.strictEqual(disemvowel("What are you, a communist?"), "Wht r y,  cmmnst?")
+    })
+  })
